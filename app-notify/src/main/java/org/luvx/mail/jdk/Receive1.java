45248211cebf239c2b67e3b1e444061086358611
F.LuVx <yeyu0926@hotmail.com>
2020-08-25 22:25:47 +0800

    update

--- app-notify/src/main/java/org/luvx/mail/jdk/Receive1.java
+++ app-notify/src/main/java/org/luvx/mail/jdk/Receive1.java
@@ -43,7 +43,62 @@ public class Receive1 {
                             + "\nSubject: " + message.getSubject()
                             + "\nFrom: " + message.getFrom()[0]
                             + "\nText: " + message.getContent().toString()
+                            + "\n发送日期: " + message.getSentDate().toInstant().atZone(ZoneOffset.ofHours(8)).toLocalDateTime()
             );
         }
     }
+
+    /**
+    获取邮件的内容(各个组成部分)
+     */
+    private static void print1(Message[] messages) throws Exception {
+        System.out.println("The count of the Email is :" + messages.length);
+        for (int i = messages.length - 1; i >= 0; i--) {
+            Message message = messages[i];
+            String title = message.getSubject();
+            if (!"TITLE".equals(title)) {
+                continue;
+            }
+            Date date = message.getSentDate();
+            LocalDate day = date.toInstant().atZone(ZoneOffset.ofHours(8)).toLocalDate();
+            if (!day.equals(LocalDate.now())) {
+                continue;
+            }
+
+            MimeMultipart content = (MimeMultipart) message.getContent();
+            int count = content.getCount();
+            System.out.println("++++++++++++" + count + "++++++++++");
+            for (int j = 0; j < count; j++) {
+                MimeBodyPart bodyPart = (MimeBodyPart) content.getBodyPart(j);
+                log.info("type:{}", bodyPart.getContentType());
+
+                if (j == 0) {
+                    MimeMultipart content1 = (MimeMultipart) bodyPart.getContent();
+                    MimeBodyPart bodyPart1 = (MimeBodyPart) content1.getBodyPart("pic_0");
+                    BASE64DecoderStream content2 = (BASE64DecoderStream) bodyPart1.getContent();
+                    Files.write(ByteStreams.toByteArray(content2), new File("D:\\work\\open\\email\\data_delivery\\" + i + ".png"));
+                    // int count1 = content1.getCount();
+                    // for (int k = 0; k < count1; k++) {
+                    //     MimeBodyPart bodyPart1 = (MimeBodyPart) content1.getBodyPart(k);
+                    //     log.info("type:{}", bodyPart1.getContentType());
+                    //     if (k == 0) {
+                    //         String content2 = (String) bodyPart1.getContent();
+                    //         log.info(content2);
+                    //     }
+                    //     if (k == 1) {
+                    //         BASE64DecoderStream content2 = (BASE64DecoderStream) bodyPart1.getContent();
+                    //         Files.write(ByteStreams.toByteArray(content2), new File("D:\\work\\open\\email\\data_delivery\\" + i + ".png"));
+                    //     }
+                    // }
+                }
+                if (j == 1) {
+                    BASE64DecoderStream content1 = (BASE64DecoderStream) bodyPart.getContent();
+                    Files.write(ByteStreams.toByteArray(content1), new File("D:\\work\\open\\email\\data_delivery\\" + i + ".pdf"));
+                }
+            }
+            System.out.println("++++++++++++++++++++++");
+        }
+    }
+
+
 }
