d4d6ea8a983a82ff7f9c01a1841dfd8e86d8deb4
F.LuVx <yeyu0926@hotmail.com>
2021-06-30 00:03:58 +0800

    update

--- usage-case/api/src/main/java/org/luvx/algorithm/array/CountNums.java
+++ usage-case/api/src/main/java/org/luvx/algorithm/array/CountNums.java
@@ -1,6 +1,6 @@
 package org.luvx.algorithm.array;
 
-import org.luvx.algorithm.search.BinarySearch;
+import java.util.Arrays;
 
 /**
  * 有序数组, 计算指定数字的出现次数
@@ -8,30 +8,20 @@ import org.luvx.algorithm.search.BinarySearch;
 public class CountNums {
     /**
      * 计算target出现的次数
-     *
-     * @param array
-     * @param target
-     * @return
      */
     public static int countNums(int[] array, int target) {
-        int index = BinarySearch.binarySearchByLoop(array, target);
+        int index = Arrays.binarySearch(array, target);
         if (index == -1) {
             return 0;
         }
-        int left = index - 1;
-        int right = index + 1;
-
-        while (array[left] == target)
+        int left = index - 1, right = index + 1;
+        while (array[left] == target) {
             left--;
-        while (array[right] == target)
+        }
+        while (array[right] == target) {
             right++;
+        }
         return right - left - 1;
-    }
-
-    public static int countNums1(int[] array, int target) {
-
-
-        return 0;
     }
 
     public static void main(String[] args) {
--- usage-case/api/src/main/java/org/luvx/algorithm/array/ModifiedArray.java
+++ usage-case/api/src/main/java/org/luvx/algorithm/array/ModifiedArray.java
@@ -1,8 +1,9 @@
 package org.luvx.algorithm.array;
 
-import org.luvx.utils.ArrayUtils;
-
 import java.util.Arrays;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 
 /**
  * 奇数在偶数的前面
@@ -11,16 +12,15 @@ import java.util.Arrays;
  * 1,3,5,2,4,6
  */
 public class ModifiedArray {
-
     public static void modifiedArray(int[] array) {
         int length = array.length;
         int i = length - 1;
         while (i >= 1) {
             if (array[i - 1] % 2 == 0 && array[i] % 2 != 0) {
-                ArrayUtils.arraySwap(array, i - 1, i);
+                ArrayUtils.swap(array, i - 1, i);
                 for (int j = i; j < length - 1; j++) {
                     if (array[j] % 2 == 0 && array[j + 1] % 2 != 0) {
-                        ArrayUtils.arraySwap(array, j, j + 1);
+                        ArrayUtils.swap(array, j, j + 1);
                     }
                 }
             }
@@ -29,7 +29,7 @@ public class ModifiedArray {
     }
 
     public static void main(String[] args) {
-        int[] array = ArrayUtils.initArray(10);
+        int[] array = {2, 1, 4, 3, 5, 6};
         System.out.println(Arrays.toString(array));
         modifiedArray(array);
         System.out.println(Arrays.toString(array));
