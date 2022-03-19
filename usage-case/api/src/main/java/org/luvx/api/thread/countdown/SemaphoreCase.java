ca1a025a7ec4696c9d3ccbdb106353fbbf694892
F.LuVx <yeyu0926@hotmail.com>
2022-03-19 20:16:12 +0800

    update

--- usage-case/api/src/main/java/org/luvx/api/thread/countdown/SemaphoreCase.java
+++ usage-case/api/src/main/java/org/luvx/api/thread/countdown/SemaphoreCase.java
@@ -1,13 +1,13 @@
 package org.luvx.api.thread.countdown;
 
-import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 例子: 银行窗口排队
@@ -26,31 +26,25 @@ public class SemaphoreCase {
     private static final int       n1        = 3;
     private static final Semaphore semaphore = new Semaphore(n1);
 
-    private static final ExecutorService executor = Executors.newFixedThreadPool(n1 + 1);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(100);
 
-    @AllArgsConstructor
-    static class A implements Runnable {
-        private final int i;
-
-        @SneakyThrows
-        @Override
-        public void run() {
-            try {
-                semaphore.acquire();
-                log.info("第{}个人开始办理业务", i);
-                TimeUnit.SECONDS.sleep(2);
-                log.info("第{}个人业务办理结束", i);
-            } finally {
-                semaphore.release();
-            }
+    @SneakyThrows
+    public void run(int i) {
+        try {
+            semaphore.acquire();
+            log.info("第{}个人业务start...", i);
+            TimeUnit.SECONDS.sleep(2);
+            log.info("第{}个人业务end...", i);
+        } finally {
+            semaphore.release();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 1; i <= num; i++) {
-            executor.execute(new A(i));
-            TimeUnit.MILLISECONDS.sleep(500);
-        }
+        SemaphoreCase exec = new SemaphoreCase();
+        IntStream.rangeClosed(1, num).forEach(i ->
+                executor.execute(() -> exec.run(i))
+        );
         executor.shutdown();
     }
 }
