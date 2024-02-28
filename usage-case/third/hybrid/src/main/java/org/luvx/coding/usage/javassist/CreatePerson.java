9c643e600a5b1977916e092c323256e97c14e92a
F.LuVx <yeyu0926@hotmail.com>
2024-02-28 20:23:04 +0800

    update

---
+++ usage-case/third/hybrid/src/main/java/org/luvx/coding/usage/javassist/CreatePerson.java
@@ -0,0 +1,54 @@
+package org.luvx.coding.usage.javassist;
+
+import javassist.*;
+
+public class CreatePerson {
+    public static void createPerson() throws Exception {
+        final String className = "org.luvx.coding.usage.javassist.Person";
+
+        ClassPool pool = ClassPool.getDefault();
+
+        // 1. 创建一个空类
+        CtClass clazz = pool.makeClass(className);
+
+        // 2. 新增一个字段 private String name;
+        // 字段名为name
+        CtField param = new CtField(pool.get("java.lang.String"), "name", clazz);
+        // 访问级别是 private
+        param.setModifiers(Modifier.PRIVATE);
+        // 初始值是 "xiaoming"
+        clazz.addField(param, CtField.Initializer.constant("foobar"));
+
+        // 3. 生成 getter、setter 方法
+        clazz.addMethod(CtNewMethod.setter("setName", param));
+        clazz.addMethod(CtNewMethod.getter("getName", param));
+
+        // 4. 添加无参的构造函数
+        CtConstructor cons = new CtConstructor(new CtClass[]{}, clazz);
+        cons.setBody("{name = \"barfoo\";}");
+        clazz.addConstructor(cons);
+
+        // 5. 添加有参的构造函数
+        cons = new CtConstructor(new CtClass[]{pool.get("java.lang.String")}, clazz);
+        // $0=this / $1,$2,$3... 代表方法参数
+        cons.setBody("{$0.name = $1;}");
+        clazz.addConstructor(cons);
+
+        // 6. 创建一个名为printName方法，无参数，无返回值，输出name值
+        CtMethod ctMethod = new CtMethod(CtClass.voidType, "printName", new CtClass[]{}, clazz);
+        ctMethod.setModifiers(Modifier.PUBLIC);
+        ctMethod.setBody("{System.out.println(name);}");
+        clazz.addMethod(ctMethod);
+
+        // 这里会将这个创建的类对象编译为.class文件
+        clazz.writeFile("/tmp/hello");
+    }
+
+    public static void main(String[] args) {
+        try {
+            createPerson();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- usage-case/third/hybrid/src/main/java/org/luvx/coding/usage/javassist/ReadByteCode.java
+++ usage-case/third/hybrid/src/main/java/org/luvx/coding/usage/javassist/ReadByteCode.java
@@ -3,27 +3,70 @@ package org.luvx.coding.usage.javassist;
 import javassist.*;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class ReadByteCode {
 
-    public static void main(String[] args) throws NotFoundException, CannotCompileException, IOException {
+    static final String classname = "org.luvx.coding.usage.javassist.Bool";
+
+    /**
+     * <pre>
+     *     运行时需要添加jvm参数
+     *     --add-opens java.base/java.lang=ALL-UNNAMED
+     * </pre>
+     */
+    public static void main(String[] args) throws Exception {
+        // aa();
+        update();
+    }
+
+    public static void update() throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass cc = pool.get(classname);
+
+        CtMethod personFly = cc.getDeclaredMethod("run");
+        personFly.insertBefore("System.out.println(\"前面\");");
+        personFly.insertAfter("System.out.println(\"后面\");");
+
+        // 新增一个方法
+        CtMethod ctMethod = new CtMethod(CtClass.voidType, "run1", new CtClass[]{}, cc);
+        ctMethod.setModifiers(Modifier.PUBLIC);
+        ctMethod.setBody("""
+                {
+                    System.out.println("执行方法中......");
+                }
+                """);
+        cc.addMethod(ctMethod);
+
+        cc.writeFile("./");
+        Object o = cc.toClass().getDeclaredConstructor().newInstance();
+
+        // 调用 personFly 方法
+        Method personFlyMethod = o.getClass().getMethod("run");
+        personFlyMethod.invoke(o);
+        //调用 joinFriend 方法
+        Method execute = o.getClass().getMethod("run1");
+        execute.invoke(o);
+    }
+
+    private static void aa() throws CannotCompileException, IOException {
         ClassPool classPool = ClassPool.getDefault();
         classPool.importPackage("org.luvx.coding.usage.javassist");
-        CtClass ctClass = null;
+        CtClass cc = null;
         try {
-            ctClass = classPool.get("org.luvx.coding.usage.javassist.Bool");
+            cc = classPool.get(classname);
         } catch (NotFoundException e) {
             e.printStackTrace();
         }
 
-        CtMethod[] methods = ctClass.getDeclaredMethods();
+        CtMethod[] methods = cc.getDeclaredMethods();
 
         for (int i = 0; i < methods.length; i++) {
             CtMethod method = methods[i];
             String methodName = method.getName();
             String oldMethodName = STR."\{methodName}$$old";
             if (!"main".equalsIgnoreCase(methodName)) {
-                CtMethod newMethod = CtNewMethod.copy(method, ctClass, null);
+                CtMethod newMethod = CtNewMethod.copy(method, cc, null);
                 method.setName(oldMethodName);
 
                 String s = STR."""
@@ -36,9 +79,9 @@ public class ReadByteCode {
 
                 newMethod.setBody(s);
                 newMethod.setName(methodName);
-                ctClass.addMethod(newMethod);
+                cc.addMethod(newMethod);
             }
         }
-        ctClass.writeFile("/tmp/hello");
+        cc.writeFile("/tmp/hello");
     }
 }
